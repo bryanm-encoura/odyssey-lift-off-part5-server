51a6ee256203fbc8e6c8acb47bea7d6235c8f2ae
Bryan Martin <108755612+bryanm-encoura@users.noreply.github.com>
2023-01-31 10:30:37 -0500

    Update schema.js

--- src/schema.js
+++ src/schema.js
@@ -35,8 +35,8 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
-    length: Int
+    "The track's approximate length to complete, in seconds"
+    length: Int @deprecated(reason:"inaccurate")
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
@@ -62,13 +62,13 @@ const typeDefs = gql`
     id: ID!
     "The module's title"
     title: String!
-    "The module's length in minutes"
-    length: Int
+    "The module's length in seconds"
+    length: Int @deprecated(reason:"inaccurate")
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
     videoUrl: String
-    dirationInSeconds
+    durationInSeconds: INT
   }
 `;
 
